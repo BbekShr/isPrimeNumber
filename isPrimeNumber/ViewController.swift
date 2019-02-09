3d4357f607ba87a8cffcb814d57a2c31e397dc9c
Bibek <bbeksh@gmail.com>
2019-02-08 20:45:43 -0600

    Initial Commit

---
+++ isPrimeNumber/ViewController.swift
@@ -0,0 +1,44 @@
+//
+//  ViewController.swift
+//  isPrimeNumber
+//
+//  Created by Bibek Shrestha on 2/8/19.
+//  Copyright © 2019 Bibek Shrestha. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var inputNum: UITextField!
+    @IBOutlet weak var showResult: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    @IBAction func buttonPressed(_ sender: Any) {
+        print("Button tapped")
+        let num = Int(inputNum.text!)
+        //let result = Int(showResult.text!)
+        if (num == 1) {
+            showResult.text = "\(num!) is not a prime number!!"
+        } else if (num == 2) || (num == 3){
+            showResult.text = "\(num!) is a prime number!!"
+        } else {
+        
+        for index in 2...(num!/2){
+            if (num! % index == 0) {
+                //print ("Not a Prime")
+                showResult.text = "\(num!) is not a prime number!!"
+            } else {
+                showResult.text = "\(num!) is a prime number!!"
+            }
+            
+            }
+        }
+    }
+    
+
+}
+
